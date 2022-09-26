2cc88a0d9a64acd5e65347d75d795165ac8bbdc7
bossLloyd <semblantelloyd500@gmail.com>
2022-09-26 20:07:43 +0800

    commit1

--- delete.php
+++ delete.php
@@ -15,6 +15,6 @@ if(isset($_GET["id"])) {
     $sql = "DELETE FROM students WHERE id = $id";
     $result = $connection->query($sql);
 }
-header("location: /school/index.php");
+header("location: ./index.php");
 exit;
 ?>
