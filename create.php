027fd798da572d80208c3346c133d2f804924b5d
print("UwU") <92197616+lloydshine@users.noreply.github.com>
2022-09-27 09:58:32 +0800

    Update create.php

--- create.php
+++ create.php
@@ -74,7 +74,7 @@
                     <button type="submit" class="btn btn-primary">Submit</button>
                 </div>
                 <div class="col-sm-3 d-grid">
-                    <a class="btn btn-outline-primary" href="/school/index.php" role="button">Cancel</a>
+                    <a class="btn btn-outline-primary" href="index.php" role="button">Cancel</a>
                 </div>
             </div>
         </form>
