a0bd3a8193b516e4023ad2ecc61e64a23ca0f845
print("UwU") <92197616+lloydshine@users.noreply.github.com>
2022-09-27 09:59:08 +0800

    Update edit.php

--- edit.php
+++ edit.php
@@ -185,7 +185,7 @@ if($_SERVER['REQUEST_METHOD'] == 'GET') {
                     <button type="submit" class="btn btn-primary">Save</button>
                 </div>
                 <div class="col-sm-3 d-grid">
-                    <a class="btn btn-outline-primary" href="/school/index.php" role="button">Cancel</a>
+                    <a class="btn btn-outline-primary" href="index.php" role="button">Cancel</a>
                 </div>
             </div>
         </form>
